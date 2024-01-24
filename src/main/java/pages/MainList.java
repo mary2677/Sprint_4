564ce786699ac45fc0942a682339ce9bc76a4fd8
Maria Karpova <alisa26.7@yandex.ru>
2024-01-24 11:30:02 +0400

    second commit

--- src/main/java/pages/MainList.java
+++ src/main/java/pages/MainList.java
@@ -46,8 +46,7 @@ public class MainList {
     }
 
     // Клик по стрелке выпадающего списка
-
-    public static void clickQuestionArrow(int questionNumber) {
+    clickQuestionArrow(int questionNumber) {
         new WebDriverWait(driver, Duration.of(5, ChronoUnit.SECONDS))
                 .until(ExpectedConditions.elementToBeClickable(By.id(questionsArray[questionNumber])));
         driver.findElement(By.id(questionsArray[questionNumber])).click();
